62d0f099dd0195dd016e5d1bc99cb2708d19cac7
Cindy Diaz <cindy.liliana.diaz@hotmail.com>
2016-05-30 15:48:33 -0400

    Updated game.ts

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,32 +1,58 @@
 /// <reference path="_reference.ts" />
-// GLOBAL GAME FRAMEWORK VARIABLES
 var canvas;
 var stage;
-var stats;
-var state;
-var scene;
-var stateFunction; // alias for our current state
-// Game Variables
 var helloLabel;
-var startButton;
 function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60); // set the frsme rate to 60 frames per second
+    createjs.Ticker.on("tick", gameLoop, this);
+}
+function gameLoop(event) {
+    stage.update();
+}
+function main() {
+    console.log("Game started");
+    helloLabel = new createjs.Text("Hello Worl", "60px Consolas", "#000000");
+    stage.addChild(helloLabel);
+}
+/*
+// GLOBAL GAME FRAMEWORK VARIABLES
+var canvas: HTMLElement;
+var stage: createjs.Stage;
+var stats: Stats;
+var state: number;
+var scene: createjs.Container;
+var stateFunction: any; // alias for our current state
+
+// Game Variables
+var helloLabel: objects.Label;
+var startButton: objects.Button;
+
+
+function init():void {
     canvas = document.getElementById("canvas"); // reference to canvas element
     stage = new createjs.Stage(canvas); // passing canvas to stage
     stage.enableMouseOver(20); // enable mouse events
     createjs.Ticker.setFPS(60); // set frame rate to 60 fps
     createjs.Ticker.on("tick", gameLoop); // update gameLoop every frame
     setupStats(); // sets up our stats counting
+
     state = config.MENU_STATE;
     changeState();
 }
+
 // Main Game Loop
-function gameLoop(event) {
+function gameLoop(event: createjs.Event): void {
     stats.begin(); // start counting
+
     stage.update(); // redraw/refresh stage every frame
+
     stats.end(); // stop counting
 }
+
 // Setup Game Stats
-function setupStats() {
+function setupStats():void {
     stats = new Stats();
     stats.setMode(0); // shows fps
     stats.domElement.style.position = "absolute";
@@ -34,17 +60,23 @@ function setupStats() {
     stats.domElement.style.top = "0px";
     document.body.appendChild(stats.domElement);
 }
+
 // Callback function / Event Handler for Start Button Click
-function clickStartButton(event) {
+function clickStartButton(event: createjs.MouseEvent): void {
     helloLabel.text = "Clicked";
 }
+
+
+
 // state machine prep
-function changeState() {
+function changeState(): void {
     // Launch various scenes
+
     switch (state) {
         case config.MENU_STATE:
             // show the menu scene
             stateFunction = states.menu;
+
             break;
         case config.PLAY_STATE:
             // show the play scene
@@ -53,6 +85,8 @@ function changeState() {
             // show the game over scene
             break;
     }
+
     stateFunction();
 }
+*/ 
 //# sourceMappingURL=game.js.map
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,5 +1,47 @@
 ﻿/// <reference path="_reference.ts" />
 
+var canvas:HTMLElement;
+var stage:createjs.Stage;
+var helloLabel:createjs.Text;
+
+function init():void {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60); // set the frsme rate to 60 frames per second
+    createjs.Ticker.on("tick",gameLoop, this);
+}
+function gameLoop(event:createjs.Event):void{
+    stage.update();
+}
+
+function main() {
+    console.log("Game started");
+    helloLabel = new createjs.Text("Hello Worl", "60px Consolas", "#000000");
+    stage.addChild(helloLabel);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // GLOBAL GAME FRAMEWORK VARIABLES
 var canvas: HTMLElement;
 var stage: createjs.Stage;
@@ -71,4 +113,4 @@ function changeState(): void {
 
     stateFunction();
 }
- 
+*/
